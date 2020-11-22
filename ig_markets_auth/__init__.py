f0d9984893584d2db3d0bd408fd8ed283f08ad41
Kevin Courbet <kevin.courbet.contact@gmail.com>
2020-11-22 17:23:34 +0100

    fix missing version

--- ig_markets_auth/__init__.py
+++ ig_markets_auth/__init__.py
@@ -1,4 +1,3 @@
-__version__ = '1.0.2'
 # flake8: noqa
 from .flow import Flow
 from .requests import AuthorizedSession
--- tests/test_ig_markets_api.py
+++ tests/test_ig_markets_api.py
@@ -2,10 +2,6 @@ import httpx
 from ig_markets_auth import __version__
 
 
-def test_version():
-    assert __version__ == '1.0.1'
-
-
 def test_api():
     import time
 
